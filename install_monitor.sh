c146cbd352dca6ea2dc3735547b317a8b7bef70a
Carlos Ivan Soto <gameue@hotmail.com>
2023-03-23 20:11:31 -0600

    sudo

--- install_monitor.sh
+++ install_monitor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Crear red de Docker para Prometheus y Grafana
-docker network create monitor-net
+sudo docker network create monitor-net
 
 # Descargar e instalar el contenedor de Prometheus
-docker run --name prometheus -d --network monitor-net -p 127.0.0.1:9090:9090 -v /path/to/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
+sudo docker run --name prometheus -d --network monitor-net -p 127.0.0.1:9090:9090 -v /path/to/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
 
 # Descargar e instalar el contenedor de Grafana
-docker run -d --name=grafana --network monitor-net -p 3000:3000 grafana/grafana-oss
+sudo docker run -d --name=grafana --network monitor-net -p 3000:3000 grafana/grafana-oss
