c969693eab60dce39c6f0b23dfffe8751fbcbed6
Carlos Ivan Soto <gameue@hotmail.com>
2023-03-23 20:09:14 -0600

    script para installar monitor en docker

--- install_monitor.sh
+++ install_monitor.sh
@@ -1,47 +1,10 @@
 #!/bin/bash
 
-# Instalar Prometheus
-echo "Instalando Prometheus..."
-wget https://github.com/prometheus/prometheus/releases/download/v2.43.0/prometheus-2.43.0.linux-amd64.tar.gz
-tar -xvf prometheus-2.43.0.linux-amd64.tar.gz
-cd prometheus-2.43.0.linux-amd64/
-sudo cp prometheus /usr/local/bin/
-sudo cp promtool /usr/local/bin/
-sudo mkdir /etc/prometheus
-sudo mkdir /var/lib/prometheus
-sudo cp -r consoles/ console_libraries/ /etc/prometheus/
-sudo cp prometheus.yml /etc/prometheus/
-sudo systemctl daemon-reload
-sudo systemctl enable prometheus
-sudo systemctl start prometheus
+# Crear red de Docker para Prometheus y Grafana
+docker network create monitor-net
 
-# Instalar Grafana
-echo "Instalando Grafana..."
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
-sudo apt-get update
-sudo apt-get install grafana
+# Descargar e instalar el contenedor de Prometheus
+docker run --name prometheus -d --network monitor-net -p 127.0.0.1:9090:9090 -v /path/to/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
 
-# Configurar Grafana
-echo "Configurando Grafana..."
-sudo systemctl daemon-reload
-sudo systemctl enable grafana-server
-sudo systemctl start grafana-server
-
-# Configurar Prometheus como fuente de datos en Grafana
-echo "Configurando Prometheus como fuente de datos en Grafana..."
-sudo cat <<EOF > /etc/grafana/provisioning/datasources/prometheus.yml
-apiVersion: 1
-datasources:
-- name: Prometheus
-  type: prometheus
-  url: http://localhost:9090
-  access: proxy
-  isDefault: true
-EOF
-
-# Reiniciar Grafana para aplicar los cambios de configuración
-echo "Reiniciando Grafana..."
-sudo systemctl restart grafana-server
-
-echo "Instalación completa."
+# Descargar e instalar el contenedor de Grafana
+docker run -d --name=grafana --network monitor-net -p 3000:3000 grafana/grafana-oss
