d295db777b8b5688e9797e935519822dfd247da6
Carlos Ivan Soto <gameue@hotmail.com>
2023-03-26 00:43:18 -0600

    wireguard client

--- wireguard-client.sh
+++ wireguard-client.sh
@@ -5,6 +5,8 @@ wg genkey | tee privatekey | wg pubkey > publickey
 
 wg set wg0 peer ./publickey allowed-ips 10.0.0.2/32
 
-
 # Agregar ruta
 ip route add 10.0.0.2/32 dev wg0
+
+# Reiniciar la interfaz de red para que tome la nueva configuración
+ifdown wg0 && ifup wg0
