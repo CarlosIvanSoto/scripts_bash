38e1fa11924b432d534bc8cd8af911bbf9b9ee6a
Carlos Ivan Soto <gameue@hotmail.com>
2023-03-26 00:27:13 -0600

    wireguard

--- wireguard.sh
+++ wireguard.sh
@@ -8,7 +8,7 @@ iface wg0 inet static
     pre-up wg setconf wg0 /etc/wireguard/wg0.conf" >> /etc/network/interfaces
 
 # Agregar el puerto a la configuración de ufw
-ufw allow 51820/udp
+sudo ufw allow 51820/udp
 
 # Reiniciar la interfaz de red para que tome la nueva configuración
-ifdown wg0 && ifup wg0
+sudo ifdown wg0 && sudo ifup wg0
